e1f289c6ceb06f97445ae99aa6734827097fc197
boB Rudis <bob@rud.is>
2018-04-04 05:46:00 -0400

    seq_in

--- R/util.R
+++ R/util.R
@@ -1,15 +1,25 @@
-seq_in <- function(source_vector, pattern_vector) {
+# seq_in <- function(source_vector, pattern_vector) {
+#
+#   which(
+#     Reduce(
+#       '+',
+#       lapply(
+#         seq_along(y <- lapply(pattern_vector, '==', source_vector)),
+#         function(x) {
+#           y[[x]][x:(length(source_vector) - length(pattern_vector) + x)]
+#         }
+#       )
+#     ) == length(pattern_vector)
+#   )
+#
+# }
 
-  which(
-    Reduce(
-      '+',
-      lapply(
-        seq_along(y <- lapply(pattern_vector, '==', source_vector)),
-        function(x) {
-          y[[x]][x:(length(source_vector) - length(pattern_vector) + x)]
-        }
-      )
-    ) == length(pattern_vector)
-  )
-
+# #ty @yeedle!
+seq_in <- function(haystack, needle) {
+  index <- seq_along(haystack)
+  for (i in seq_along(needle)) {
+    pile <- haystack[index]
+    index <- index[pile==needle[i]] + 1L
+  }
+  (index-length(needle))[1]
 }
